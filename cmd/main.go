0e0ed17800bcbbf97effba6587298aa36703e5a4
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2025-05-31 12:36:17 +0300

    wip: 2025-05-31 12:36:10

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "embed"
-	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -20,32 +20,60 @@ var devPrompt string
 //go:embed prompts/raw.txt
 var rawPrompt string
 
+type config struct {
+	debug         bool
+	mode          string
+	openRouterKey string
+}
+
+func (c *config) read() {
+	c.debug = os.Getenv("DEBUG") == "1" || os.Getenv("DEBUG") == "true"
+	c.mode = os.Getenv("MODE")
+	c.openRouterKey = os.Getenv("OPENROUTER_KEY")
+}
+
 func main() {
+	var cfg config
+	cfg.read()
+	// validate OpenRouter key
+	if cfg.openRouterKey == "" {
+		log.Fatal("OPENROUTER_KEY environment variable is not set")
+	}
+	// setup the Docker container for running bash commands
 	if err := buildBashDockerIfNeeded(); err != nil {
-		fmt.Printf("error building docker image: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("error building bash docker image: %v", err)
 	}
-	if err := os.MkdirAll(".logs", 0755); err != nil {
-		panic(fmt.Sprintf("error creating debug folder: %v", err))
+	// if in debug mode, create a debug log file
+	var debugLogger logger.Logger = logger.NoOp()
+	if cfg.debug {
+		if err := os.MkdirAll(".ikm/logs", 0755); err != nil {
+			log.Fatalf("error creating debug folder: %v", err)
+		}
+		debugLogFile := time.Now().Format("2006-01-02T15:04:05") + ".log"
+		f, err := os.OpenFile(".ikm/logs/"+debugLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalf("error opening log file: %v", err)
+		}
+		defer f.Close() //nolint:errcheck
+		debugLogger = logger.New(f)
+		debugLogger.SetEnabled(true)
+		debugLogger.SetLevel("debug")
 	}
-	debugLogFile := "debug-" + time.Now().Format("2006-01-02T15:04:05") + ".log"
-	f, err := os.OpenFile(".logs/"+debugLogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(fmt.Sprintf("error opening log file: %v", err))
+	// init the terminal UI model and run the program
+	if cfg.mode == "" {
+		cfg.mode = "raw"
 	}
-	defer f.Close() //nolint:errcheck
-	logger := logger.New(f)
-	logger.SetEnabled(true)
-	logger.SetLevel("debug")
-	model := tui.Initial(logger, os.Getenv("OPENROUTER_API_KEY"), runInBashDocker,
+	if cfg.mode != "agent" && cfg.mode != "dev" && cfg.mode != "raw" {
+		log.Fatalf("invalid MODE environment variable: %s, must be one of: agent, dev, raw", cfg.mode)
+	}
+	model := tui.Initial(debugLogger, cfg.openRouterKey, runInBashDocker,
 		tui.WithStaticMode("agent", agentPrompt),
 		tui.WithStaticMode("dev", devPrompt),
 		tui.WithStaticMode("raw", rawPrompt),
-		tui.WithSetDefaultMode("dev"),
+		tui.WithSetDefaultMode(cfg.mode),
 	)
 	program := tea.NewProgram(model, tea.WithAltScreen())
 	if _, err := program.Run(); err != nil {
-		fmt.Printf("error running program: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("error running program: %v", err)
 	}
 }
