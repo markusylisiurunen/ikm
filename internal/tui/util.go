1e29253ab4abf3911a4ac88493577d22fc546d21
Markus Ylisiurunen <markus.ylisiurunen@gmail.com>
2025-06-01 23:58:19 +0300

    wip: 2025-06-01 23:58:16

--- internal/tui/util.go
+++ internal/tui/util.go
@@ -10,74 +10,58 @@ func wrapLine(s string, width int) []string {
 		return []string{s}
 	}
 	var result []string
-	var currentLine strings.Builder
-	curlen := 0
-	words := strings.Split(s, " ")
-
-	for i, word := range words {
+	var curline strings.Builder
+	var curlen int = 0
+	for i, word := range strings.Split(s, " ") {
 		wlen := utf8.RuneCountInString(word)
-
-		// Handle the first word or if the word fits on the current line
 		if i == 0 {
 			if wlen <= width {
-				currentLine.WriteString(word)
+				curline.WriteString(word)
 				curlen += wlen
 			} else {
-				// Word is too long, split it at character boundaries
 				lines := splitLongWord(word, width)
 				result = append(result, lines[:len(lines)-1]...)
-				currentLine.WriteString(lines[len(lines)-1])
+				curline.WriteString(lines[len(lines)-1])
 				curlen = utf8.RuneCountInString(lines[len(lines)-1])
 			}
 		} else if curlen+1+wlen <= width {
-			// Word fits on current line with a space
-			currentLine.WriteByte(' ')
-			currentLine.WriteString(word)
+			curline.WriteByte(' ')
+			curline.WriteString(word)
 			curlen += 1 + wlen
 		} else {
-			// Word doesn't fit, start a new line
-			result = append(result, currentLine.String())
-			currentLine.Reset()
-
+			result = append(result, curline.String())
+			curline.Reset()
 			if wlen <= width {
-				currentLine.WriteString(word)
+				curline.WriteString(word)
 				curlen = wlen
 			} else {
-				// Word is too long, split it at character boundaries
 				lines := splitLongWord(word, width)
 				result = append(result, lines[:len(lines)-1]...)
-				currentLine.WriteString(lines[len(lines)-1])
+				curline.WriteString(lines[len(lines)-1])
 				curlen = utf8.RuneCountInString(lines[len(lines)-1])
 			}
 		}
 	}
-
-	if currentLine.Len() > 0 {
-		result = append(result, currentLine.String())
+	if curline.Len() > 0 {
+		result = append(result, curline.String())
 	}
-
 	return result
 }
 
-// splitLongWord splits a word that's longer than width at character boundaries
 func splitLongWord(word string, width int) []string {
 	if width <= 0 {
 		return []string{word}
 	}
-
 	var lines []string
 	runes := []rune(word)
-
 	for len(runes) > 0 {
 		if len(runes) <= width {
 			lines = append(lines, string(runes))
 			break
 		}
-
 		lines = append(lines, string(runes[:width]))
 		runes = runes[width:]
 	}
-
 	return lines
 }
 
